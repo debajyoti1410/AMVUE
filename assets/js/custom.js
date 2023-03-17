0b9795297048c5cc413887b8a0589c88a3277185
debajyoti1410 <debajyotisen.babu@gmail.com>
2023-03-17 18:46:38 +0530

    top button add

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,19 +1,32 @@
 
+
+let mybutton = document.getElementById("myBtn");
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+         mybutton.style.display = "block";
+       } else {
+         mybutton.style.display = "none";
+       }
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
+    
     if ($(this).scrollTop() > 50) {
       document.getElementById("navbar").style.background = "white";
-  } else {
+    } else {
       document.getElementById("navbar").style.background = "transparent";
-  }
+    }
   } else {
     document.getElementById("navbar").style.top = "-100px";
+    mybutton.style.display = "block";
   }
   prevScrollpos = currentScrollPos;
 }
+function topFunction() {
+     document.body.scrollTop = 0;
+     document.documentElement.scrollTop = 0;
+   }
 
 $('.containers').animatedHeadline({
   animationType: "slide",
@@ -47,3 +60,23 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// Get the button
+// let mybutton = document.getElementById("myBtn");
+
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () { scrollFunction() };
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
